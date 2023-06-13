07e7ec698fb2bd8792a61dbddf343ff0424371a6
heyroocoke <heyroocoke@gmail.com>
2023-06-13 21:17:53 +0900

    💄 input 입력폼 작업

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,23 +1,29 @@
 import React from "react";
+import { styled } from "styled-components";
 
 const SignIn = () => {
+  const LoginContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `;
   return (
-    <div>
-      <h2>Login</h2>
+    <LoginContainer>
+      <h2>로그인</h2>
       <div>
-        <label htmlFor="input_id">EMAIL : </label>
+        <label htmlFor="input_id">이메일 : </label>
         <input type="text" name="input_id" data-testid="email-input" />
       </div>
       <div>
-        <label htmlFor="input_pw">PW : </label>
+        <label htmlFor="input_pw">패스워드 : </label>
         <input type="password" name="input_pw" data-testid="password-input" />
       </div>
       <div>
         <button type="button" data-testid="signin-button">
-          Login
+          로그인
         </button>
       </div>
-    </div>
+    </LoginContainer>
   );
 };
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const SignUp = () => {
-  return <div></div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const inputEmailHandle = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const inputPasswordHandle = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const signUpHandle = () => {
+    axios
+      .post("http://localhost:8000/auth/signup", {
+        email: email,
+        password: password,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <h2>회원가입</h2>
+      <div>
+        <label htmlFor="input_email">이메일 : </label>
+        <input
+          type="text"
+          name="input_email"
+          data-testid="email-input"
+          value={email}
+          onChange={inputEmailHandle}
+        />
+      </div>
+      <div>
+        <label htmlFor="input_pw">패스워드 : </label>
+        <input
+          type="password"
+          name="input_pw"
+          data-testid="password-input"
+          value={password}
+          onChange={inputPasswordHandle}
+        />
+      </div>
+      <div>
+        <button
+          type="button"
+          data-testid="signup-button"
+          onClick={signUpHandle}
+        >
+          회원가입
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
